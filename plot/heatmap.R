9664a2302c709f3b5eae63d51cf96b720c3a8335
Sid <siddhart005@e.ntu.edu.sg>
2017-01-19 19:52:17 +0800

    update to plot

--- plot/heatmap.R
+++ plot/heatmap.R
@@ -15,14 +15,23 @@ data <- merge(geocodes,time,by=c("postcode"))
 #remove NAs
 data <- data[complete.cases(data),]
 
+#limit max time to 10hours (because there are postcodes for pulau ubin, which takes 30hrs to walk! completely skews the results!)
+data <- data[data$time<36000,]
+
+#compute cycling time
+data$cycle_time <- (data$time/3)/3600 # in hours
+
+# Set a bounding box for fetching the map
+lat <- c(1.252874,1.478786)                
+lon <- c(103.596471,104.023215)   
+
 # fetch Singapore map
-sg_map <- get_map(location = "Singapore", zoom = 11)
+sg_map <- get_map(location = c(lon = mean(lon), lat = mean(lat)), zoom = 11, source = "google", color="bw")
 
-# plot heat map using ggmap
+# plot heat map using ggmap -- limit lon and lat axes
 pdf("singapore.pdf",width=20,height=12)
-ggmap(sg_map,extent = "device") +
-#	geom_density2d(data = data,aes(x = lon, y = lat), size = 0.3) + 
-	stat_density2d(data = data, aes(x = lon, y = lat, fill = ..level.., alpha = ..level..), size = 0.01, bins = 16, geom = "polygon") + 
-	scale_fill_gradient(low = "green", high = "red") + 
-	scale_alpha(range = c(0, 0.3), guide = FALSE)
-
+ggmap(sg_map) +
+  scale_x_continuous(limits = lon, expand = c(0, 0)) +
+  scale_y_continuous(limits = lat, expand = c(0, 0)) +
+  geom_point(aes(x = lon, y = lat, colour = cycle_time), data = data, alpha = .1) +
+  scale_colour_gradient(low = "green", high = "red",guide="colourbar")
